038a163ac6cd73599e1fca3ea523992b0e3fe7db
namhbcf1 <bangAchieu2@gmail.com>
2025-05-25 20:16:18 +0700

    Reset to stable version with fixed compatibility between CPU, motherboard and RAM

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const path = require('path');
 const cors = require('cors');
-const performanceCalculator = require('./performance-calculator.js');
+// const performanceCalculator = require('./performance-calculator.js');
 
 const app = express();
 const port = process.env.PORT || 3000;
